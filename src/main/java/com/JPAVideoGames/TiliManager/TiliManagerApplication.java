50ff3bea94500d44c67461fc0d1e4d7d9dccd00d
Pau Ojeda Gallego <pojeda@elpuig.xeill.net>
2026-02-19 17:09:23 +0100

    Primer Commit

---
+++ src/main/java/com/JPAVideoGames/TiliManager/TiliManagerApplication.java
@@ -0,0 +1,13 @@
+package com.JPAVideoGames.TiliManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiliManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiliManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JPAVideoGames/TiliManager/TiliManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPAVideoGames.TiliManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiliManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
