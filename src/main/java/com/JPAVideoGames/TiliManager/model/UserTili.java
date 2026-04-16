2bc61012d8f845307102253d5b2f6dd9ea2bff03
Pau Ojeda Gallego <pojeda@elpuig.xeill.net>
2026-04-16 23:59:32 +0200

    first 18 Raimon players

--- src/main/java/com/JPAVideoGames/TiliManager/model/UserTili.java
+++ src/main/java/com/JPAVideoGames/TiliManager/model/UserTili.java
@@ -1,8 +1,5 @@
 package com.JPAVideoGames.TiliManager.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 
 @Entity
